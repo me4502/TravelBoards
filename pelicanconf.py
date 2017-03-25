7dfd7ff089e4dc33b4d765b01f1ea1269564193a
Matthew Miller <mnmiller1@me.com>
2017-03-25 17:29:54 +1000

    Budget page

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,7 +31,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = False
 
-DIRECT_TEMPLATES = ['index', 'people', 'map']
+DIRECT_TEMPLATES = ['index', 'people', 'map', 'budget']
 
 THEME = "theme"
 
