7c5635fe6b0ab81915be64dce51307b114c8654b
Matthew Miller <mnmiller1@me.com>
2017-03-25 16:00:51 +1000

    Use theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,5 +31,9 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = False
 
+DIRECT_TEMPLATES = ['index']
+
+THEME = "theme"
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
