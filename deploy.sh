f0270e4fb6fa5f1f739c9a500a32b52a3d1713ae
Matthew Miller <mnmiller1@me.com>
2017-03-25 16:02:05 +1000

    Updated deploy (reverted from commit c0463fa5099f475df18f6315f59a160c0ae45c09)

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,18 @@ echo -e "$VARNAME"
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo -e "Starting to deploy to Github Pages\n"
-    cd $PELICAN_OUTPUT_FOLDER
     if [ "$TRAVIS" == "true" ]; then
         git config --global user.email "travis@travis-ci.org"
         git config --global user.name "Travis"
     fi
-
-    git init
-    git remote add origin https://Me4502:${GH_TOKEN}@github.com/$TARGET_REPO >/dev/null
-    git checkout --orphan $BRANCH
-    git add .
-    git commit -q -m "Deploy $(date)" &> /dev/null
-    git push -q -f origin $BRANCH &> /dev/null
-
+    #using token clone gh-pages branch
+    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
+    #go into directory and copy data we're interested in to that directory
+    cd built_website
+    rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
+    #add, commit and push files
+    git add -f .
+    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
+    git push -fq origin $BRANCH > /dev/null
     echo -e "Deploy completed\n"
 fi
