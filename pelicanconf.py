d5c780dd2f21eb8075b2fff298edbe05b8da91e3
Matthew Miller <mnmiller1@me.com>
2017-03-26 10:26:35 +1000

    Added next/prev buttons to cloud and timeline.

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,16 +19,6 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = False
 
 DIRECT_TEMPLATES = ['index', 'people', 'map', 'budget', 'timeline',
